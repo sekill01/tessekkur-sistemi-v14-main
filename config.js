a22518c935ea9fbf09ac9461c528b53dac9289fb
sekill01 <80636880+sekill01@users.noreply.github.com>
2023-08-03 01:23:48 +0300

    tessekkur-sistemi-v14-main

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  //////////////////////////
+  token: "MTExOTU1NTM3NjQ2NDAxNTQwMg.GXU0Dp.DhlQ4lBxlHKXz0UpZ8oHBRkZ2GmEEz3YEezjK0", //WRITE YOUR BOT TOKEN
+  botStatus: "RoxenCraft", //WRITE YOUR BOT STATUS.
+  log: "", //log kanal id yazın
+  aways: [
+    {
+      role: "1129152871858962498", //rol id yazın ödül rolü
+      thanks_size: 10 //kaç teşekkürle alınacak
+    },
+    {
+      role: "1129152871858962498", //rol id yazın ödül rolü
+      thanks_size: 25 //kaç teşekkürle alınacak
+    },
+    {
+      role: "1129152871858962498", //rol id yazın ödül rolü
+      thanks_size: 50 //kaç teşekkürle alınacak
+    },
+    {
+      role: "1129152871858962498", //rol id yazın ödül rolü
+      thanks_size: 100 //kaç teşekkürle alınacak
+    },
+  ]
+  //////////////////////////
+}
